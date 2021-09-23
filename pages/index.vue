16ec0c2919c8b5936784f1d7e610ae52346c919b
r0ckandc0de <yohtaro.s@gmail.com>
2021-09-23 05:29:44 +0000

    プリロード画像を変更

--- pages/index.vue
+++ pages/index.vue
@@ -6,8 +6,8 @@
     <NuxtLink :to="`/movie/${topMovie[0].id}/`">
       <div class="relative invisible ">
         <!-- preload="none" preload="metadata" にするか -->
-        <div class="absolute video-wrapper inset-0 transition duration-500 opacity-100">
-          <video class="visible video" preload="none" poster="/images/preload.png" autoplay loop muted playsinline webkit-playsinline="true">
+        <div class="absolute inset-0 transition duration-500 opacity-100">
+          <video class="visible video" preload="none" poster="/images/preload2.png" autoplay loop muted playsinline webkit-playsinline="true">
             <source src="~/assets/video/short_sample_alex.mp4" type="video/mp4">
           </video>
         </div>
