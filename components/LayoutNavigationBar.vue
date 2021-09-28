0efa522974778e1a597d47b5148d5869c9f3995e
r0ckandc0de <yohtaro.s@gmail.com>
2021-09-28 10:22:54 +0000

    Mapタグを追加

--- components/LayoutNavigationBar.vue
+++ components/LayoutNavigationBar.vue
@@ -20,6 +20,17 @@
         <div class="text-xxs m-auto">Search</div>
       </div>
     </NuxtLink>
+    <NuxtLink to="/map">
+      <div class="flex flex-col py-1">
+        <div class="md:px-5 px-3 pt-1">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
+          </svg>
+        </div>
+        <div class="text-xxs m-auto">Map</div>
+      </div>
+    </NuxtLink>
       <!-- <div class="md:p-5 p-3 delay-75 duration-500 ease-in-out transform hover:scale-125 hover:-rotate-12">
           <svg xmlns="http://www.w3.org/2000/svg" class="text-cBlack h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
---
+++ pages/map/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <!-- layoutここから -->
+  <layout-wrapper>
+    <layout-tab />
+    <p>hogehoge</p>
+
+  <!-- layoutここまで -->
+  </layout-wrapper>
+</template>
+
+<script>
+export default {
+
+  data() {
+    return {
+      
+    };
+  },
+  async asyncData({ $microcms }) {
+    const movies = await $microcms.get({
+      endpoint: 'movie',
+      queries: { limit: 10 },
+    });
+
+    return{
+      movies: movies.contents,
+
+    }
+  }
+}
+
+</script>
