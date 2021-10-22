e4b3581b7b459d85a91c25ffa22e75ab0793e29a
r0ckandc0de <yohtaro.s@gmail.com>
2021-10-22 09:33:35 +0000

    ローカルエリア情報を日本語化

--- pages/area/_id.vue
+++ pages/area/_id.vue
@@ -11,9 +11,19 @@
       </li>
     </ul>
     <div v-if="$i18n.locale === 'ja'">
-      <pre class="text-cMain">{{ area }}</pre>
-      <div v-html="area[0].map"></div>
+      <!-- <pre class="text-cMain">{{ area }}</pre> -->
+      <div v-html="area[0].news" class="news"></div>
+      <div v-html="area[0].facility" class="facility"></div>
+      <div v-html="area[0].map" class="googleMap"></div>
+      <div v-html="area[0].twitter" class="twitter"></div>
+      <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+      <div id="fb-root"></div>
+      <script async defer crossorigin="anonymous" src="https://connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v12.0" nonce="NyahAgtx"></script>
+      <div class="fb-page" data-href="https://www.facebook.com/freeclimbjp" data-tabs="timeline" data-width="" data-height="315" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="false"><blockquote cite="https://www.facebook.com/freeclimbjp" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/freeclimbjp">日本フリークライミング協会</a></blockquote></div>
+      <!-- <span v-html="area[0].facebook" class="facebook" ></span> -->
     </div>
+
     <div v-if="$i18n.locale !== 'ja'">
       <layout-movie-list-wrapper>
           <base-heading>{{ area[0].name }}</base-heading>
